b57e5220d69119fd0daf3079e517a0ed897a56fd
Shayden seymour <shaydenseymour@gmail.com>
2025-06-23 09:25:00 +1200

    String to number utility

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -2,23 +2,3 @@
 #include "Utilities.h"
 #include <concepts>
 #include <optional>
-
-template<typename T>
-requires std::integral<T> || std::floating_point<T>
-std::optional<T> bwss::utilities::convert::stringToNumber(const std::string& str) noexcept {
-  try {
-    if constexpr (std::integral<T>) {
-      if constexpr (std::signed_integral<T>) {
-        return static_cast<T>(std::stoll(str));
-      } else {
-        return static_cast<T>(std::stoull(str));
-      }
-    } else if constexpr (std::floating_point<T>) {
-      return static_cast<T>(std::stod(str));
-    }
-  } catch (...) {
-    return std::nullopt;
-  }
-
-  return std::nullopt;
-}
--- src/include/Utilities.h
+++ src/include/Utilities.h
@@ -7,6 +7,22 @@ namespace bwss::utilities {
   namespace convert {
     template<typename T>
     requires std::integral<T> || std::floating_point<T>
-    std::optional<T> stringToNumber(const std::string& str) noexcept;
+    std::optional<T> stringToNumber(const std::string& str) noexcept {
+      try {
+        if constexpr (std::integral<T>) {
+          if constexpr (std::signed_integral<T>) {
+            return static_cast<T>(std::stoll(str));
+          } else {
+            return static_cast<T>(std::stoull(str));
+          }
+        } else if constexpr (std::floating_point<T>) {
+          return static_cast<T>(std::stod(str));
+        }
+      } catch (...) {
+        return std::nullopt;
+      }
+
+      return std::nullopt;
+    }
   }
 }
