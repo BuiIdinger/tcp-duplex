9afcb785f53fc37bc8723adc66be9f38212dff18
Shayden seymour <shaydenseymour@gmail.com>
2025-06-30 02:22:00 +1200

    Better network setup and options

--- src/Network.cpp
+++ src/Network.cpp
@@ -11,127 +11,139 @@
 #include "Bwss.h"
 #include <unistd.h>
 #include <fcntl.h>
+#include <utility>
+#include <netinet/tcp.h>
 
-void network::sock::allowReuseAddress() {
-  static constexpr int reuse = 1;
-  if (setsockopt(httpSocket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
+std::pair<bool, int32_t> bwss::network::sock::setCongestionControl(const int32_t& sock) noexcept {
+  constexpr const char* bbr = "bbr";
+  constexpr const char* cubic = "cubic";
+  constexpr const char* reno = "reno";
+
+  if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, bbr, strlen(bbr)) == 0) {
+    return { true, errno };
+  }
+  if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, cubic, strlen(cubic)) == 0) {
+    return { true, errno };
+  }
+  if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, reno, strlen(reno)) == 0) {
+    return { true, errno };
   }
 
-  if (setsockopt(httpsSocket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
-  }
+  return { false, 0 };
 }
 
-void network::sock::create() {
-  httpSocket = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
-  if (httpSocket == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
+std::pair<bool, int32_t> bwss::network::sock::setDeferAcceptTimeout(const int32_t& sock) noexcept {
+  if (setsockopt(sock, IPPROTO_TCP, TCP_DEFER_ACCEPT, &deferAcceptTimeout, sizeof(deferAcceptTimeout)) == -1) {
+    return { false, errno };
   }
 
-  httpsSocket = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
-  if (httpsSocket == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
-  }
+  return { true, 0 };
 }
 
-void network::sock::allowDualStackSocket() {
-  static constexpr int flag = 1;
-  if (setsockopt(httpSocket, IPPROTO_IPV6, IPV6_V6ONLY, &flag, sizeof(flag)) == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
+std::pair<bool, int32_t> bwss::network::sock::setNoDelay(const int32_t& sock) noexcept {
+  if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &enableNoDelay, sizeof(enableNoDelay)) == -1) {
+    return { false, errno };
   }
 
-  if (setsockopt(httpsSocket, IPPROTO_IPV6, IPV6_V6ONLY, &flag, sizeof(flag)) == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
-  }
+  return { true, 0 };
 }
 
-static void configureAddress(sockaddr_in6& address, const int& port) {
-  memset(&address, 0, sizeof(address));
+std::pair<bool, int32_t> bwss::network::sock::setKeepAlive(const int32_t& sock) noexcept {
+  if (setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &enableTCPKeepAlive, sizeof(enableTCPKeepAlive)) == -1) {
+    return { false, errno };
+  }
+
+  /*
+   * Three system calls are avoided here, as their default values can be changed in /etc/sysctl.conf
+   *
+   * net.ipv4.tcp_keepalive_time = N    // N amount of seconds before keep-alive probes start
+   * net.ipv4.tcp_keepalive_intvl = N   // N amount of seconds between of intervals between keep-alive probes
+   * net.ipv4.tcp_keepalive_probes = N  // N amount of probes that can fail before connection is considered dead
+   */
+
+  return { true, 0 };
+}
+
+std::pair<bool, int32_t> bwss::network::sock::setFastOpen(const int32_t& sock) noexcept {
+  if (setsockopt(sock, IPPROTO_TCP, TCP_FASTOPEN, &enableTCPFastOpen, sizeof(enableTCPFastOpen)) == -1) {
+    return { false, errno };
+  }
+
+  return { true, 0 };
+}
+
+std::pair<bool, int32_t> bwss::network::sock::setReuse(const int32_t& sock) noexcept {
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enableReuseAddress, sizeof(enableReuseAddress)) == -1) {
+    return { false, errno };
+  }
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &enableReusePort, sizeof(enableReusePort)) == -1) {
+    return { false, errno };
+  }
+
+  return { true, 0 };
+}
+
+std::pair<bool, int32_t> bwss::network::sock::create() noexcept {
+  const int32_t sock = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
+  if (sock == -1) {
+    return { false, errno };
+  }
+
+  return { true, sock };
+}
+
+std::pair<bool, int32_t> bwss::network::sock::setDualStack(const int32_t& sock) noexcept {
+  constexpr int32_t flag = 0;
+  if (setsockopt(sock, IPPROTO_IPV6, IPV6_V6ONLY, &flag, sizeof(flag)) == -1) {
+    return { false, errno };
+  }
+
+  return { true, 0 };
+}
+
+void bwss::network::sock::configureAddress(sockaddr_in6& address, const int32_t& port) noexcept {
+  std::memset(&address, 0, sizeof(address));
   address.sin6_family = AF_INET6;
   address.sin6_addr = in6addr_any;
   address.sin6_port = htons(port);
+  address.sin6_flowinfo = 0;
+  address.sin6_scope_id = 0;
 }
 
-void network::sock::configureServerAddresses() {
-  configureAddress(httpSocketAddress, httpPort);
-  configureAddress(httpsSocketAddress, httpsPort);
-}
-
-static bool bindSock(const int sock, sockaddr_in6 address) {
+std::pair<bool, int32_t> bwss::network::sock::bind(const int32_t& sock, sockaddr_in6& address) noexcept {
   if (bind(sock, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == -1) {
-    std::cerr << std::strerror(errno) << std::flush;
-    return false;
+    return { false, errno };
   }
 
-  return true;
+  return { true, 0 };
 }
 
-void network::sock::bind() {
-  if (!bindSock(httpSocket, httpSocketAddress)) {
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
+std::pair<bool, int32_t> bwss::network::sock::listen(const int32_t& sock) noexcept {
+  if (::listen(sock, 0) == -1) {
+    return { false, errno };
   }
 
-  if (!bindSock(httpsSocket, httpsSocketAddress)) {
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
-  }
+  return { true, 0 };
 }
 
-static bool listenSock(const int sock) {
-  if (listen(sock, 0) == -1) {
-    std::cerr <<  std::strerror(errno) << std::flush;
-    return false;
+std::pair<bool, int32_t> bwss::network::sock::close(int32_t& sock) noexcept {
+  if (sock != -1) {
+    ::close(sock);
+    sock = -1;
+    return { true, 0 };
   }
 
-  return true;
+  return { false, 0 };
 }
 
-void network::sock::listen() {
-  if (!listenSock(httpSocket)) {
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
-  }
-
-  if (!listenSock(httpsSocket)) {
-    exit(EXIT_FAILURE);
-    UNREACHABLE;
-  }
-}
-
-void network::sock::shutdown() {
-  if (httpSocket != -1) {
-    close(httpSocket);
-    httpSocket = -1;
-  }
-
-  if (httpsSocket != -1) {
-    close(httpsSocket);
-    httpsSocket = -1;
-  }
-}
-
-bool network::sock::setNonBlocking(const int& fd) {
-  const int flags = fcntl(fd, F_GETFL, 0);
+std::pair<bool, int32_t> bwss::network::sock::setNonBlocking(const int32_t& sock) noexcept {
+  const int32_t flags = fcntl(sock, F_GETFL, 0);
   if (flags == -1) {
-    return false;
+    return { false, errno };
   }
-  if (fcntl(fd, F_SETFL, flags | O_NONBLOCK)) {
-    return false;
+  if (fcntl(sock, F_SETFL, flags | O_NONBLOCK)) {
+    return { true, errno };
   }
 
-  return true;
+  return { true, 0 };
 }
--- src/include/Network.h
+++ src/include/Network.h
@@ -7,25 +7,102 @@
 #pragma once
 
 #include <netinet/in.h>
+#include <utility>
 
-namespace network {
-  namespace sock {
-    inline int httpSocket;
-    inline int httpsSocket;
+namespace bwss::network::sock {
+  /*
+   * Ports that clients can connect to
+   *
+   * Don't change these for a public facing server.
+   * A users router may have a firewall rule to block non-web
+   * ports, causing the connection attempt to fail
+   */
+  constexpr int32_t httpPort = 80;
+  constexpr int32_t httpsPort = 443;
 
-    constexpr int httpPort = 80;
-    constexpr int httpsPort = 443;
+  /*
+   * TCP keep-alive
+   *
+   * This enables keep-alive at the TCP level when there is no data being sent after N
+   * amount of seconds, these probes will detect dead connections and alert us.
+   *
+   * While WebSockets do use custom ping/pong frames, we could still get a TCP connection
+   * being dead at the WebSocket upgrade stage. Since we are not upgraded to a WebSocket, we
+   * cannot use WebSocket ping/pong frames
+   */
+  constexpr int32_t enableTCPKeepAlive = true;
+  constexpr int32_t idle = 10;      // Start probes after 10 seconds idle
+  constexpr int32_t interval = 5;   // Send probes every 5 seconds
+  constexpr int32_t maxFailed = 3;  // Fail after 3 unanswered probes
 
-    inline sockaddr_in6 httpSocketAddress;
-    inline sockaddr_in6 httpsSocketAddress;
+  /*
+   * TCP fast open
+   *
+   * TCP fast open allows data to be sent during the initial SYN packet of the
+   * TCP handshake.
+   *
+   * Normally:        SYN → SYN-ACK → ACK → DATA
+   * TCP fast open:   SYN → SYN-ACK → ACK
+   *
+   * TCP fast open requires the system setting to be enabled
+   *
+   * No major browsers support TCP fast open anymore as issues with middlebox compatibility
+   * and privacy Concerns, so I would suggest TCP fast open should stay off.
+   */
+  constexpr int32_t enableTCPFastOpen = false;
 
-    void create();
-    void allowReuseAddress();
-    void allowDualStackSocket();
-    void configureServerAddresses();
-    void bind();
-    void listen();
-    void shutdown();
-    bool setNonBlocking(const int& fd);
-  }
+  /*
+   * Reuse
+   *
+   * Reuse address allows the program to bind to a port that is in a TIME_WAIT state. If your program
+   * crashes and attempts to restart without the address reuse, then you'll probably get the
+   * 'Address already in use' error.
+   *
+   * Port reuse allows multiple sockets to bind to the same port simultaneously. This allows for the kernel
+   * to load balance incoming connections to different sockets on different threads. This is what allows
+   * multithreading to work in BWSS, so don't disable it.
+   */
+  constexpr int32_t enableReuseAddress = true;
+  constexpr int32_t enableReusePort = true;
+
+  /*
+   * Dual-stack
+   *
+   * Dual-stack allows a socket to support both IPV4 and IPV6 clients, rather than needing
+   * two sockets, one for IPV4 and IPV6.
+   */
+  constexpr int32_t enableIPV6Only = false;
+
+  /*
+   * No delay
+   *
+   * TCP sockets by default use Nagle's algorithm to buffer up smaller messages into a larger
+   * message to reduce network congestion, and wasted bandwidth. Nagle's algorithm delays messages
+   * being sent which can introduce latency.
+   *.
+   * Enabling no delay will not buffer up any messages, and will send them immediately
+   */
+  constexpr int32_t enableNoDelay = false;
+
+  /*
+   * Defer accept timeout
+   *
+   * Defer accept timeout will defer the accept wakeup call to the eventing system if the client doesn't
+   * send a packet before the timeout.
+   */
+  constexpr int32_t deferAcceptTimeout = 0;
+
+  std::pair<bool, int32_t> setCongestionControl(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> setDeferAcceptTimeout(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> setNoDelay(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> setKeepAlive(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> setFastOpen(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> create() noexcept;
+  std::pair<bool, int32_t> setReuse(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> setDualStack(const int32_t& sock) noexcept;
+  void configureAddress(sockaddr_in6& address, const int32_t& port) noexcept;
+  std::pair<bool, int32_t> bind(const int32_t& sock, sockaddr_in6& address) noexcept;
+  std::pair<bool, int32_t> listen(const int32_t& sock) noexcept;
+  std::pair<bool, int32_t> close(int32_t& sock) noexcept;
+  std::pair<bool, int32_t> setNonBlocking(const int32_t& fd) noexcept;
 }
